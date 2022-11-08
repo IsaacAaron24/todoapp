dadfea9b1ff0641de744cdb6eb074ac78802202a
IsaacAaron24 <isaacaaron272@gmail.com>
2022-11-08 07:36:07 -0500

    correccion1

--- src/Controllers/users.controllers.js
+++ src/Controllers/users.controllers.js
@@ -53,12 +53,12 @@ const createUser = async(req, res) => {
 
 const updateUser = async(req, res) => {
     try {
-        const {id} = req.params
-        const updateData = req.body
-        const result = await UserServices.update(updateData, id)
-        res.status(200).json(result) 
+        const {id} = req.params;
+        const dataUpdate = req.body;
+        const result = await UserServices.update(dataUpdate, id);
+        res.status(200).json(result); 
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
